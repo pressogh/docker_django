413823d311014a3bf899d0908fef31040839db42
PRESSO <user1205@naver.com>
2022-04-21 16:38:53 +0900

    Jenkins Test6

--- docker_django/testpage/views.py
+++ docker_django/testpage/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def MainView(request):
-    return HttpResponse("Jenkins Auto Build!!!!")
+    return HttpResponse("Jenkins Auto Build!!!!!!")
     
