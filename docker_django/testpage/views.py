8f0ca967559139e32ff0149ee5876e49555a5550
PRESSO <user1205@naver.com>
2022-04-21 18:15:36 +0900

    T12

--- docker_django/testpage/views.py
+++ docker_django/testpage/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def MainView(request):
-    return HttpResponse("Jenkins Auto Build #11")
+    return HttpResponse("Jenkins Auto Build #12")
     
